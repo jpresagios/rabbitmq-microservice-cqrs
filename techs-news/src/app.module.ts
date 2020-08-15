0fb9c1649b67b4c362c0b79384675cf916bf9793
Junior <junior.presagios@gmail.com>
2020-08-14 21:00:07 -0400

    Added app module

--- techs-news/src/app.module.ts
+++ techs-news/src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { NewsModule } from './news/news.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import env from './configs/env-config';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [NewsModule, MongooseModule.forRoot(env.MONGODB_URI)],
 })
 export class AppModule {}
