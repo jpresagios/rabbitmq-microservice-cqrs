32c73684b71da28e60f3af7fb0dce225d3b8b942
Junior <junior.presagios@gmail.com>
2020-08-14 21:00:17 -0400

    Added main module

--- techs-news/src/main.ts
+++ techs-news/src/main.ts
@@ -1,9 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule } from '@nestjs/swagger';
+import { swaggerOptions } from './configs/swagger.config';
 import env from './configs/env-config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const document = SwaggerModule.createDocument(app, swaggerOptions);
+  SwaggerModule.setup('/swagger', app, document);
+
   await app.listen(env.API_PORT);
 }
 bootstrap();
