11b6d9a2b926deb2c871cc08f38b56bfd8f838ba
Junior <junior.presagios@gmail.com>
2020-08-15 11:08:10 -0400

    Added main file and expose swagger doc

--- sports-news/src/main.ts
+++ sports-news/src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule } from '@nestjs/swagger';
+import { swaggerOptions } from './configs/swagger.config';
+import env from './configs/env-config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const document = SwaggerModule.createDocument(app, swaggerOptions);
+  SwaggerModule.setup('/swagger', app, document);
+
+  await app.listen(env.API_PORT);
 }
 bootstrap();
