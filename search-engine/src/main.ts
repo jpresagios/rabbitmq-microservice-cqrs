7ea9fdb72780a0a43da582815c5a6fb17f8ca98b
Junior <junior.presagios@gmail.com>
2020-08-15 12:49:09 -0400

    Added main file

--- search-engine/src/main.ts
+++ search-engine/src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Transport } from '@nestjs/microservices';
+import env from './configs/env-config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  app.connectMicroservice({
+    transport: Transport.RMQ,
+    options: {
+      urls: [env.AMQP_URL],
+      queue: 'news_queue',
+      noAck: false,
+      queueOptions: { durable: false },
+    },
+  });
+
+  await app.startAllMicroservicesAsync();
+
+  await app.listen(env.API_PORT);
 }
 bootstrap();
